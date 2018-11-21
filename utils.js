f0b47a4a353ef7c5bc26fb9aa7269ac2eebfafd7
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2018-11-21 22:11:59 +0700

    Revert "[Utils] Update utils.js"
    This reverts commit 6ccff408f1d9d8784103b3780d6b114df4e88296.

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
-var minAge = "Thu, 01 Jan 1970 00:00:00 UTC", maxAge = "Fri, 31 Dec 9999 23:59:59 UTC";
+var maxAge = "Fri, 31 Dec 9999 23:59:59 UTC";
 
 var setCookie = function (name, value) {
-    document.cookie = name + "=" + JSON.stringify(value) + ";expires=" + maxAge + ";path=/;";
+    document.cookie = name + "=" + JSON.stringify(value) + ";expires=" + maxAge;
 };
 
 var getCookie = function (name) {
@@ -26,10 +26,6 @@ var getCookie = function (name) {
     return "";
 };
 
-var deleteCookie = function (name) {
-    document.cookie = name + "=;expires=" + minAge + ";path=/;";
-};
-
 var numericSort = function (a, b) {
     return b - a;
 };
@@ -47,18 +43,6 @@ var sep = function (number) {
 
 String.prototype.contains = function (string) {
     return this.indexOf(string) > -1;
-};
-
-String.prototype.cap = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-};
-
-String.prototype.removeSpaces = function () {
-    return this.replace(/ /g, "");
-};
-
-String.prototype.strip = function () {
-    return this.replace(/<\/?[^>]*>/g, "");
 };
 
 Object.defineProperty(Array.prototype, "contains", {
